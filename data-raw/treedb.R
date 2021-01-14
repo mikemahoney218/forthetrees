22ebc5283ed7a638dee1ae6f1e06a58a689b7e97
Mike Mahoney <mike.mahoney.218@gmail.com>
2021-01-14 12:01:12 -0500

    Build out lookup tables

--- data-raw/treedb.R
+++ data-raw/treedb.R
@@ -7,11 +7,11 @@ tree_defs <- vapply(
   character(1)
 )
 tree_defs <- data.frame(
-  id = gsub("\\.txt$", "", names(tree_defs)),
-  call = tree_defs,
+  render_key = gsub("\\.txt$", "", names(tree_defs)),
+  render_call = tree_defs,
   row.names = NULL
 )
 
 con <- DBI::dbConnect(duckdb::duckdb(), dbdir = "inst/extdata/treedb.db")
-DBI::dbWriteTable(con, "tree_defs", tree_defs, overwrite = TRUE)
+DBI::dbWriteTable(con, "sapling_tree_defs", tree_defs, overwrite = TRUE)
 DBI::dbDisconnect(con, shutdown = TRUE)
