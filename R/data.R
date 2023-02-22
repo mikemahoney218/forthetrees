b3574bd5c9ae305eb0520da22a34dc67c4f063c8
Mike Mahoney <mike.mahoney.218@gmail.com>
2023-02-22 14:05:46 -0500

    Document data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Tree positions
+#'
+#' @format A data frame with 1279 observations of 4 values:
+#' \describe{
+#'   \item{EAST}{Eastings, in meters from the lower-left corner}
+#'   \item{NORTH}{Northings, in meters from the lower-left corner}
+#'   \item{SPECIES}{A numeric code indicating what species a tree is}
+#'   \item{DBH85}{Diameter at breast height, in inches}
+#' }
+"tree_positions"
